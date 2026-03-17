dbe5ada43cf295729e67312a96f4ffc2e4b6c376
pinarqs <mailin@gmail.com>
2026-03-17 20:19:27 +0300

    first commit

---
+++ database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./chat.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+# ✅ BURAYA EKLE (önceden eksikti)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Text
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+
+class Chat(Base):
+    __tablename__ = "chats"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    user_message = Column(Text)
+    ai_response = Column(Text)
